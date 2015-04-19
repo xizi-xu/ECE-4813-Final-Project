9c281ef24c91af2ccc6af1af597e4396b810154c
Liuxizi Xu <xlxzbisulyn@gmail.com>
2015-04-19 18:27:01 -0400

    clean up

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 python mrsample.py test1.csv >result.txt  -q
 python textTocsv.py
-python mrsample.py test1.csv >resultPaul.txt  -q
+python mostCommonPhrase.py test1.csv >resultPaul.txt  -q
 python textTocsv2.py
